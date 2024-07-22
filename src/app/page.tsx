d18d600fade1c4691e3959e90754e59d8c36098e
Kayla Lin <klin23@u.rochester.edu>
2024-07-22 07:55:08 -0400

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,6 @@ export default function Home() {
     <main className="bg-[#18181B] flex flex-col gap-8 ">
       <h1 className="text-white">Idle animation after finish playing</h1>
       <Example source="/theo-with-idle.riv" />
-      <h1 className="text-white">No idle animation after finish playing</h1>
-      <Example source="/theo-no-idle.riv" />
     </main>
   );
 }
