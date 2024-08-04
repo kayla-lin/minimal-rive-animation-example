d1c02bda4fbe23f1ebf0fd5742aec676378afb56
Kayla Lin <klin23@u.rochester.edu>
2024-08-04 00:49:48 -0400

    title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ export default function Home() {
   return (
     <main className="bg-[#18181B] flex flex-col gap-8 ">
       <h1 className="text-white">Turn on this animation</h1>
-      <h1 className="text-white">Fade into animation</h1>
+      <h1 className="text-white">Fade into animation (V2)</h1>
       <Example source="/file_upload_load.riv" artboard="V2" />
-      <h1 className="text-white">Idle animation after finish playing</h1>
+      <h1 className="text-white">Old animation (V1)</h1>
       <Example source="/theo-with-idle.riv" />
     </main>
   );
